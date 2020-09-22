a4ef7676bf3fe6b52e2bf67fc5635e3e217bf8c3
first commit <>
2020-09-23 02:39:08 +0900

    game clear,overなどの表示 ステージ生成

---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+    float speed = 20.0f;
+    private Rigidbody rigidbody;
+
+    public GameObject gameclearCanvas;
+    bool gameclear = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+
+        if (this.transform.position.z > 170)
+        {
+            gameclear = true;
+            gameclearCanvas.SetActive(true);
+
+            if (this.transform.position.y < -60)
+            {
+                SceneManager.LoadScene("title");
+            }
+        }
+       
+    }
+
+}
---
+++ Assets/Script/gameover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class gameover : MonoBehaviour
+{
+
+    bool GAMEOVER = false;
+    public GameObject gameoverCanvas;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject player = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GAMEOVER = true;
+            gameoverCanvas.SetActive(true);
+            Destroy(player);
+        }
+       
+    }
+}
